68e48991e3e81cbf2637bbe457059afb339ad11f
blaff <haritsaardli@gmail.com>
2021-09-07 17:50:39 +0700

    edit

--- latihan.js
+++ latihan.js
@@ -1,8 +1,38 @@
-const form = document.getElementById("form")
+const form = document.getElementById("formData")
 function showHide() {
     if (form.style.display === "none") {
         form.style.display = "flex"
     } else {
         form.style.display = "none"
     }
+}
+
+function test (){
+    const nama = document.getElementById("namaInput")
+    const role = document.getElementById("roleInput")
+    const avability = document.getElementById("avabilityInput")
+    const usia = document.getElementById("usiaInput")
+    const lokasi = document.getElementById("lokasiInput")
+    const experiance = document.getElementById("experianceInput")
+    const email = document.getElementById("emailInput")
+    const password = document.getElementById("passwordInput")
+
+    const namaTmpl = document.getElementById("nama")
+    const roleTmpl = document.getElementById("role")
+    const avabilityTmpl = document.getElementById("avability")
+    const usiaTmpl = document.getElementById("usia")
+    const lokasiTmpl = document.getElementById("lokasi")
+    const experienceTmpl = document.getElementById("experiance")
+    const emailTmpl = document.getElementById("email")
+
+    namaTmpl.innerHTML = nama.value
+    roleTmpl.innerHTML = role.value
+    avabilityTmpl.innerHTML = avability.value
+    usiaTmpl.innerHTML = usia.value
+    lokasiTmpl.innerHTML = lokasi.value
+    experienceTmpl.innerHTML = experiance.value
+    emailTmpl.innerHTML = email.value
+
+    
+    document.getElementById("edit").reset()
 }
